46d79e17215c923a7a32975b8695eabbb1217d65
jjbing1201 <jjbing726@gmail.com>
2014-12-23 18:05:47 +0800

    20141223R1
    - add ：新增小宝书和NP调用页面
    - add :   主页面排版结束

--- MainViewController.swift
+++ MainViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class MainViewController : UIViewController {
+class MainViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var UserMainTableView: UITableView!
     
     // MARK: - MainViewController 全局内容
     override func viewDidLoad() {
@@ -20,5 +22,29 @@ class MainViewController : UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    // MARK: - 用户tableview override部分内容
+    
+    /* 固定1部分内容 */
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    /* 固定1部分中含有5行内容 */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        var label = UILabel()
+        label.text = ""
+        return label
+    }
+    
+    /* 固定5部分内容的样式，遵循模拟样式 */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "UserMainCell")
+        return cell
+    }
+    
 
 }
---
+++ ShopController.swift
@@ -0,0 +1,42 @@
+//
+//  ShopController.swift
+//  NaturalPark
+//
+//  Created by Computer on 14/12/23.
+//  Copyright (c) 2014 Computer. All rights reserved.
+//
+
+import UIKit
+
+class ShopController: UIViewController {
+    
+    @IBOutlet weak var Shopwv: UIWebView!
+    
+    // MARK: - ShopController全局内容
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 设置导航栏标题
+        self.title="返回";
+        let replyItem=UIBarButtonItem(title:"返回", style:.Plain, target:self, action:"replyFrom");
+        //  添加到到导航栏上
+        self.navigationItem.leftBarButtonItem = replyItem;
+        
+        // 下一级展示页面
+        let fturl = NSURL(string: "http://www.baidu.com")
+        Shopwv?.loadRequest(NSURLRequest(URL: fturl!))
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func replyFrom() {
+        // 转场
+        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
